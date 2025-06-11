1dd5ecd76fbf58c237dc2a88e190a492cf6a70c5
Miki <ml227cr@student.lnu.se>
2025-06-11 14:33:46 +0200

    add LED activation upon physical button press

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-# Import from libraries
 import time
-from machine import Pin
+from machine import Pin, ADC
+from math import log
 
-# Set the OUTPUT pin to on-board LED
-led = Pin("LED", Pin.OUT)
+# Configure GPIO 0 (labeled GP0 on Pico pinout, physical pin 1) as output for the LED.
+# First argument refers to GP0, not the physical pin (1).
+led = Pin(0, Pin.OUT)
 
-# Runs forever
+# Pin.IN tells the MCU this is an input to read from
+# PULL_UP turns on an internal resistor that keeps pin high (1) when button is not pressed, and 0 when pressed.
+button = Pin(15, Pin.IN, Pin.PULL_UP)
+
+# Activate LED blinking loop
+# while True:
+#     led.value(1)
+#     time.sleep(0.2)
+#     led.value(0)
+#     time.sleep(0.3)
+
+# Activate LED with button press
 while True:
-    led.on()              # Turn on LED
-    time.sleep(0.2)       # Delay for 0.2 seconds
-    led.off()             # Turn off LED
-    time.sleep(1.0)       # Delay for 1.0 seconds
+    if button.value() == 0:
+        led.on() # same as led.value(1)
+    else:
+        led.off() # same as led.value(0)
+        time.sleep(0.01) # short pause
